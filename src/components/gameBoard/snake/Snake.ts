54c5882884f810aa777c852e1155019cf3415c26
C-Puqing <c.puqing@gmail.com>
2024-02-06 16:38:31 +0800

    fix: 调整蛇头的横纵坐标为图片中心点

--- src/components/gameBoard/snake/Snake.ts
+++ src/components/gameBoard/snake/Snake.ts
@@ -26,37 +26,30 @@ class Snake {
     this.reset();
   }
 
+  // 取图片中心点的横坐标作为蛇的横坐标
   get X() {
-    return this.head.offsetLeft;
+    return this.head.offsetLeft + this.head.width / 2;
   }
 
+  // 取图片中心点的横坐标作为蛇的横坐标
   get Y() {
-    return this.head.offsetTop;
-  }
-
-  // 设置蛇头的横坐标
-  set X(value: number) {
-    this.head.style.left = value + 'px';
-  }
-
-  set Y(value: number) {
-    this.head.style.top = value + 'px';
+    return this.head.offsetTop + this.head.height / 2;
   }
 
   // 移动，向面向移动10px
   move() {
     switch (this.direction) {
       case 'up':
-        this.Y -= STEP;
+        this.head.style.top = this.head.offsetTop - STEP + 'px';
         break;
       case 'down':
-        this.Y += STEP;
+        this.head.style.top = this.head.offsetTop + STEP + 'px';
         break;
       case 'left':
-        this.X -= STEP;
+        this.head.style.left = this.head.offsetLeft - STEP + 'px';
         break;
       case 'right':
-        this.X += STEP;
+        this.head.style.left = this.head.offsetLeft + STEP + 'px';
         break;
     }
 
@@ -78,7 +71,7 @@ class Snake {
   // 碰撞检测，检查头和身子的位置是否相等，若相等就是撞到一起了
   checkCollision(): boolean {
     // 如果蛇头撞到了边界，返回true
-    if (this.X < 0 || this.X >= this.boundaryWidth || this.Y < 0 || this.Y >= this.boundaryHeight) {
+    if (this.X <= 0 || this.X >= this.boundaryWidth || this.Y <= 0 || this.Y >= this.boundaryHeight) {
       return true;
     }
     for (let i = 1; i < this.element.children.length; i++) {
@@ -94,10 +87,11 @@ class Snake {
   public reset() {
     // 清除定时器
     if (this.intervalId) {
-      clearInterval(this.intervalId);
+      window.clearInterval(this.intervalId);
     }
-    this.X = this.boundaryWidth / 2;
-    this.Y = this.boundaryHeight / 2;
+    // 图片位于面板中间
+    this.head.style.left = (this.boundaryWidth - this.head.width) / 2 + 'px';
+    this.head.style.top = (this.boundaryHeight - this.head.height) / 2 + 'px';
     // 清除所有身子
     for (let i = 1; i < this.element.children.length; i++) {
       this.element.removeChild(this.element.children[i]);
